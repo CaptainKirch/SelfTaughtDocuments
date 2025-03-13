d52bbb2e775bc1e86cca6c28af6a4b469aec5f40
LiamKircher <kircherliam@gmail.com>
2025-03-13 09:48:40 -0700

    Day 2 Py

---
+++ Daily Challenges/Week 1/day2.py
@@ -0,0 +1,17 @@
+# Day 2 Medium Python Questions and Challenges
+
+# Question #1 --- 1️⃣ Two Sum
+#Given an array of integers nums and an integer target, return the indices of the two numbers that add up to target.
+
+def two_sum(nums, target):
+    
+    
+
+
+
+
+
+
+
+print(two_sum([2, 7, 11, 15], 9))  # Output: [0, 1]
+
