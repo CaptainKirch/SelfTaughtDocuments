b5ebff504d6d92ebc1ef9a7baed982c8285e7502
LiamKircher <kircherliam@gmail.com>
2025-03-15 09:07:27 -0700

    Anki WarmUp

---
+++ Daily Anki Challenges/day2anki.py
@@ -0,0 +1,39 @@
+#Anki #1 ------------------
+
+# my_list = [12,5,9,21,7]
+
+# my_list.sort(reverse=True)
+
+# print(my_list[0])
+
+#Anki #2 ------------------
+
+# from typing import Counter
+
+# def duplicate_val(x):
+    
+#     count = Counter(x)
+
+#     return [num for num in x if count[num] == 1]
+
+
+# print(duplicate_val([1,2,2,5,5,3,6]))
+
+
+#Anki #3 ------------------
+
+# my_str = "python cool"
+
+# new_str = my_str.title()
+
+# print(new_str)
+
+
+
+
+
+
+
+
+
+
---
+++ Python - #SlidingWindowProblems/second5.py
@@ -0,0 +1,25 @@
+# Day 2 - Sliding Window Problems Challenge Questions - Writing PseduoCode and Syntax
+
+# Problem 1 - Given an integer array nums and an integer k, return the maximum sum of any contiguous subarray of size k.
+
+def max_sum_subarray(nums, k):
+    
+
+
+
+
+
+
+
+
+print(max_sum_subarray([2, 1, 5, 1, 3, 2], 3))  # Output: 9 (subarray [5,1,3])
+print(max_sum_subarray([1, 9, 3, 7, 2], 2))  # Output: 10 (subarray [9,3])
+
+
+
+
+
+
+
+
+
