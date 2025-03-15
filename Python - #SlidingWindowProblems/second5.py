9fac3ce3a8f351b627013eb62a15ea0e0e976f24
LiamKircher <kircherliam@gmail.com>
2025-03-15 12:21:01 -0700

    Sliding Window 3

--- Python - #SlidingWindowProblems/second5.py
+++ Python - #SlidingWindowProblems/second5.py
@@ -69,25 +69,43 @@
 
 # Problem 3 - Given an array of positive integers nums and an integer k, find the maximum product of any contiguous subarray of size k.
 
+# from math import prod
 
-def max_product_subarray(nums, k):
+# def max_product_subarray(nums, k):
+#     window_product = prod(nums[:k])
+#     max_product = window_product
 
-    # Create variables for the sum of K and max_sum of the sliding window.
-    window_sum = sum(nums[:k])
-    max_sum = window_sum
+#     for i in range(k, len(nums)):
+#         if nums [i - k] != 0:
+#             window_product = (window_product // nums[i - k] * nums[i])
+#         else:
+#             window_product = prod(nums[i-k+1:i+1])
 
-    # Create a for loop that loops over the array starting from the K position to the entire range of the array.
-    for i in range(k, len(nums)):
-        max_sum += nums[i] - nums[i - k]  # Update the max_sum based on the sliding window.
-        max_sum = max(max_sum, window_sum)  # Update the max_sum based on the sliding window.
+#         max_product = max(max_product, window_product)
+    
+#     return max_product
 
-    return     #Return the max sum.
+# print(max_product_subarray([1, 5, 2, 3, 6], 2))  # Output: 18 (subarray [3,6])
+# print(max_product_subarray([4, 3, 2, 5, 8], 3))  # Output: 80 (subarray [2,5,8])
+# print(max_product_subarray([2, 1, 1, 2, 4], 2))  # Output: 8 (subarray [2,4])
 
 
-print(max_product_subarray([1, 5, 2, 3, 6], 2))  # Output: 18 (subarray [3,6])
-print(max_product_subarray([4, 3, 2, 5, 8], 3))  # Output: 80 (subarray [2,5,8])
-print(max_product_subarray([2, 1, 1, 2, 4], 2))  # Output: 8 (subarray [2,4])
+# Problem 3 - Given an array of integers nums and an integer k, find the minimum sum of any contiguous subarray of size k.
 
+# def min_sum_subarray(nums, k):
+
+#     window_sum = sum(nums[:k])
+#     min_sum = window_sum
+
+#     for i in range(k, len(nums)):
+#         window_sum += nums[i] - nums[i - k]
+#         min_sum = min(min_sum, window_sum)
+
+#     return min_sum
+
+# print(min_sum_subarray([2, 1, 5, 1, 3, 2], 3))  # Output: 6 (subarray [1,3,2])
+# print(min_sum_subarray([4, 3, 2, 5, 8], 2))  # Output: 5 (subarray [3,2])
+# print(min_sum_subarray([10, 20, 30, 40, 50], 3))  # Output: 60 (subarray [10,20,30])
 
 
 
