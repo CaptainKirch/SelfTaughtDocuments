88222d4549b68bff2fd62eba99ed9c5cc25dc1a7
LiamKircher <kircherliam@gmail.com>
2025-03-18 10:58:03 -0700

    sliding window problems 2

--- Python - #SlidingWindowProblems/third5.py
+++ Python - #SlidingWindowProblems/third5.py
@@ -15,11 +15,22 @@ print(fixed_window([1, 12, -5, -6, 50, 3],4))
 
 # Sliding Window Easy Problem 2: “Given an array of positive integers and a target ‘S,’ find the length of the longest contiguous subarray whose sum is less than or equal to ‘S’. Return 0 if no such subarray exists.”
 
-def dynamic_wind(arr,S):
-    
+def longest_subarray_with_sum_at_most(arr, S):
+    window_sum = 0
+    max_length = 0  # Track longest subarray
+    window_start = 0
 
+    for window_end in range(len(arr)):
+        window_sum += arr[window_end]  # Expand window
 
+        # If sum exceeds S, shrink from left
+        while window_sum > S:
+            window_sum -= arr[window_start]
+            window_start += 1
+        
+        # Update longest subarray length
+        max_length = max(max_length, window_end - window_start + 1)
 
+    return max_length  # Return 0 if no valid subarray exists
 
-# arr = [4, 3, 1, 2, 5, 1, 6]
-# S = 7
+print(longest_subarray_with_sum_at_most([4, 3, 1, 2, 5, 1, 6], 7))  # Output: 4
