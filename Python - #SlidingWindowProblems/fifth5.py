b4e505b7b918c742b17e5aed2664e17ef07bff4c
LiamKircher <kircherliam@gmail.com>
2025-03-20 14:07:25 -0700

    sliding

--- Python - #SlidingWindowProblems/fifth5.py
+++ Python - #SlidingWindowProblems/fifth5.py
@@ -28,7 +28,6 @@
 
 # Given an array of positive integers nums and an integer target, find the length of the smallest contiguous subarray whose sum is greater than or equal to target. If no such subarray exists, return 0.
 
-
 # import math
 
 # def smallest_subarray(nums,target):
@@ -38,12 +37,81 @@
 
 #     for right in range(len(nums)):
 #         current_sum += nums[right]
-        
 
 #         while current_sum >= target:
 #             min_length = min(min_length, right - left +1)
 #             current_sum -= nums[left]
 #             left += 1
             
-            
 #     return 0 if min_length == math.inf else min_length
+
+
+# def longest_subarray(nums,target):
+#     max_length = 0
+#     left = 0
+#     current_sum = 0
+    
+#     for right in range(len(nums)):
+#         current_sum += nums[right]
+
+#         while current_sum > target:
+#             current_sum -= nums[left]
+#             left += 1
+        
+#         max_length = max(max_length, right - left + 1)
+#     return max_length
+
+
+# def longest_substring(s):
+#     max_length = 0
+#     char_count = {}
+#     left = 0
+
+#     for right in range(len(s)):
+#         char_count[s[right]] = char_count.get(s[right], 0 ) + 1
+
+#         while char_count[s[right]] > 1:
+#             char_count[s[left]] -= 1
+#             if char_count[s[left]] == 0:
+#                 del char_count[s[left]]
+#             left += 1
+
+#         max_length = max(max_length, right - left + 1)
+        
+#     return max_length
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
